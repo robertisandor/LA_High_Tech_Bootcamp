f47d8505fb2f9c1e903c8e3632af37dc9274eb63
Robert Sandor <robert.i.sandor@gmail.com>
2017-03-14 17:51:40 +0000

    Refactored html, imgs and jQuery

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,14 +1,20 @@
-$("#aboutButton").click(function() {
-  $('html,body').animate({
-    scrollTop: $("#about").offset().top}, 'slow');
-});
+$(document).ready(function() {
+  alert("jquery is working!");
+  $("#aboutButton").click(function() {
+    $('html,body').animate({
+      scrollTop: $("#about").offset().top
+    }, 'slow');
+  });
 
-$("#portfolioButton").click(function() {
-  $('html,body').animate({
-    scrollTop: $("#portfolio").offset().top}, 'slow');
-});
+  $("#portfolioButton").click(function() {
+    $('html,body').animate({
+      scrollTop: $("#portfolio").offset().top
+    }, 'slow');
+  });
 
-$("#contactButton").click(function() {
-  $('html,body').animate({
-    scrollTop: $("#contact").offset().top}, 'slow');
+  $("#contactButton").click(function() {
+    $('html,body').animate({
+      scrollTop: $("#contact").offset().top
+    }, 'slow');
+  });
 });
