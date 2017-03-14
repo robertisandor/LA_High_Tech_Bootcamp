57e531c1d88cba65fd5d1b10595cb399e068e641
Robert Sandor <robert.i.sandor@gmail.com>
2017-03-14 07:46:35 +0000

    Added portfolio html, css, & js

---
+++ docs/js/index.js
@@ -0,0 +1,14 @@
+$("#aboutButton").click(function() {
+  $('html,body').animate({
+    scrollTop: $("#about").offset().top}, 'slow');
+});
+
+$("#portfolioButton").click(function() {
+  $('html,body').animate({
+    scrollTop: $("#portfolio").offset().top}, 'slow');
+});
+
+$("#contactButton").click(function() {
+  $('html,body').animate({
+    scrollTop: $("#contact").offset().top}, 'slow');
+});
